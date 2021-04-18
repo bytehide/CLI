98ca2e997a3eb69a30aae6f8ac5b4298722d71db
juanal98 <32484496+juanal98@users.noreply.github.com>
2021-04-18 23:48:26 +0200

    cleanup commands

--- src/Commands/ShieldCommands.cs
+++ src/Commands/ShieldCommands.cs
@@ -1,20 +1,15 @@
-﻿using Microsoft.Extensions.Configuration;
-using Shield.Client;
-using Shield.Client.Models;
-using Shield.Client.Models.API.Application;
-using Shield.Client.Models.API.Project;
-using ShieldCLI.Repos;
-using Spectre.Console;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
 using System.Threading.Tasks;
 using Shield.Client.Extensions;
+using Shield.Client.Models;
 using ShieldCLI.Helpers;
+using ShieldCLI.Repos;
+using Spectre.Console;
 
 namespace ShieldCLI.Commands
 {
@@ -22,14 +17,14 @@ namespace ShieldCLI.Commands
 
 
     {
-        private ClientManager ClientManager { get; set; }
-        private DependenciesResolver DependenciesResolver { get; set; }
-
         public ShieldCommands(ClientManager clientManager, DependenciesResolver dependenciesResolver)
         {
             ClientManager = clientManager;
             DependenciesResolver = dependenciesResolver;
         }
+
+        private ClientManager ClientManager { get; }
+        private DependenciesResolver DependenciesResolver { get; }
 
         //public void saludoShield()
         //{
@@ -40,36 +35,26 @@ namespace ShieldCLI.Commands
 
 
         /// <summary>
-        /// Open DotnetSafer web to register a new user
+        ///     Open DotnetSafer web to register a new user
         /// </summary>
         public void AuthRegister()
         {
             //ClientManager.Client.Configuration.
 
-            this.OpenBrowser("https://my.dotnetsafer.com/register");
-            return;
-
+            OpenBrowser("https://my.dotnetsafer.com/register");
         }
 
         public void OpenBrowser(string url)
         {
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-            {
-                Process.Start(new ProcessStartInfo(url) { UseShellExecute = true }); // Works ok on windows
-            }
+                Process.Start(new ProcessStartInfo(url) {UseShellExecute = true}); // Works ok on windows
             else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-            {
-                Process.Start("xdg-open", url);  // Works ok on linux
-            }
-            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
-            {
-                Process.Start("open", url); // Not tested
-            }
-
+                Process.Start("xdg-open", url); // Works ok on linux
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX)) Process.Start("open", url); // Not tested
         }
 
         /// <summary>
-        /// Log in the current user whit an apiKey.
+        ///     Log in the current user whit an apiKey.
         /// </summary>
         /// <param name="apiKey">APIKEY of Dotnetsafer to use the CLI</param>
         public void AuthDoLogin(string apiKey)
@@ -83,63 +68,48 @@ namespace ShieldCLI.Commands
 
             if (ClientManager.IsValidKey(apiKey))
             {
-
                 ClientManager.UpdateKey(apiKey);
                 AnsiConsole.Markup("[lime]Logged in Correctly [/]");
-
             }
             else
             {
-
                 AnsiConsole.Markup("[red]NOT logged in. Please review the API Key[/]");
             }
-
-
         }
 
         /// <summary>
-        /// Checks if user is logged in.
+        ///     Checks if user is logged in.
         /// </summary>
         public bool AuthHasCredentials()
         {
+            if (ClientManager.HasValidClient()) return true;
 
-            if (!ClientManager.HasValidClient())
-            {
-                AnsiConsole.MarkupLine("[red]You are NOT logged in. \nYou must be logged in to use Dotnetsafer.[/]");
+            AnsiConsole.MarkupLine("[red]You are NOT logged in. \nYou must be logged in to use Dotnetsafer.[/]");
+            Console.WriteLine("");
 
+            if (!AnsiConsole.Confirm("[blue]Do you want to logged in now? [/]"))
+                return false;
 
-                Console.WriteLine("");
-                if (!AnsiConsole.Confirm("[blue]Do you want to logged in now? [/]"))
-                {
-                    return false;
-                }
-
-                Console.WriteLine("");
-                AuthDoLogin(null);
-            }
-
+            Console.WriteLine("");
+            AuthDoLogin(null);
 
             return true;
         }
 
         /// <summary>
-        /// Log out and clear credentials or current user. 
+        ///     Log out and clear credentials or current user.
         /// </summary>
         public void AuthClearCredentials()
         {
-            if (!AnsiConsole.Confirm("[red]This action will DELETE your credentials. Are you sure? [/]"))
-            {
-                return;
-            }
+            if (!AnsiConsole.Confirm("[red]This action will DELETE your credentials. Are you sure? [/]")) return;
             ClientManager.ClearClient();
             Console.WriteLine("");
             AnsiConsole.Markup("[red]Credentials deleted. You must to loggin again to use ShieldCLI [/]");
-            return;
         }
 
 
         /// <summary>
-        /// Find the config files in a paht
+        ///     Find the config files in a paht
         /// </summary>
         /// <param name="type"></param>
         /// <param name="path"></param>
@@ -147,49 +117,36 @@ namespace ShieldCLI.Commands
         /// <param name="create"></param>
         public void ConfigGetFile(string type, string path, string name, bool create)
         {
-
-
             if (type != "application" && type != "project")
-            {
                 type = AnsiConsole.Prompt(
-                         new SelectionPrompt<string>()
-                         .Title("[white]Please choose the type of protection[/]?")
-                         .PageSize(3)
-                         .AddChoice("project")
-                         .AddChoice("application"));
-            }
+                    new SelectionPrompt<string>()
+                        .Title("[white]Please choose the type of protection[/]?")
+                        .PageSize(3)
+                        .AddChoice("project")
+                        .AddChoice("application"));
 
             var fullFilePath = $"{path}/shield.{type}.{name}.json";
 
 
             if (type == "application")
-            {
                 ClientManager.Client.Configuration.LoadApplicationConfigurationFromFileOrDefault(fullFilePath);
-                //if (!File.Exists(fullFilePath))
-               //     SaveToFile(path, name);
-
-
-            }
+            //if (!File.Exists(fullFilePath))
+            //     SaveToFile(path, name);
             else
-            {
                 ClientManager.Client.Configuration.LoadProjectConfigurationFromFileOrDefault(fullFilePath);
-            }
-
-
-
         }
 
         internal async Task<List<(string, string)>> ResolveDependenciesAsync(string applicationPath)
         {
             var (isValid, requiredDependencies, (module, createdContext)) =
-                        await DependenciesResolver.GetAssemblyInfoAsync(applicationPath ?? string.Empty);
+                await DependenciesResolver.GetAssemblyInfoAsync(applicationPath ?? string.Empty);
 
             if (!isValid)
                 throw new Exception("Invalid .NET Assembly");
 
             var requiredDep = requiredDependencies.ToList();
 
-            AnsiConsole.Markup($"[green]Resolving dependencies locally...[/]");
+            AnsiConsole.Markup("[green]Resolving dependencies locally...[/]");
             AnsiConsole.WriteLine();
 
             var dependencies = DependenciesResolver.GetUnresolved(module,
@@ -200,10 +157,10 @@ namespace ShieldCLI.Commands
             await AnsiConsole.Progress().Columns(
                     new ProgressColumn[]
                     {
-                                new TaskDescriptionColumn(),
-                                new ProgressBarColumn(),
-                                new PercentageColumn(),
-                                new RemainingTimeColumn()
+                        new TaskDescriptionColumn(),
+                        new ProgressBarColumn(),
+                        new PercentageColumn(),
+                        new RemainingTimeColumn()
                     }
                 )
                 .StartAsync(async context =>
@@ -217,9 +174,9 @@ namespace ShieldCLI.Commands
                         var info = Utils.SplitAssemblyInfo(assembly);
 
                         await DependenciesResolver.GetUnresolvedWithNuget(
-                                module,
-                                createdContext, requiredDep, info.name,
-                                info.version);
+                            module,
+                            createdContext, requiredDep, info.name,
+                            info.version);
 
                         resolverTask.Increment(1);
                     }
@@ -229,7 +186,8 @@ namespace ShieldCLI.Commands
             {
                 var unresolved = requiredDep.Where(dep => string.IsNullOrEmpty(dep.Item2)).ToList();
 
-                AnsiConsole.Markup($"The following dependencies [red]({unresolved.Count})[/] are required to process the application:");
+                AnsiConsole.Markup(
+                    $"The following dependencies [red]({unresolved.Count})[/] are required to process the application:");
                 AnsiConsole.WriteLine();
 
                 var table = new Table();
@@ -247,16 +205,19 @@ namespace ShieldCLI.Commands
 
                 AnsiConsole.Render(table);
 
-                unresolved.ForEach(dep => userPath.Add(AnsiConsole.Ask<string>($"Enter the path of the [yellow]{Utils.SplitAssemblyInfo(dep.Item1).name}[/] library:")));
+                unresolved.ForEach(dep =>
+                    userPath.Add(AnsiConsole.Ask<string>(
+                        $"Enter the path of the [yellow]{Utils.SplitAssemblyInfo(dep.Item1).name}[/] library:")));
 
                 _ = DependenciesResolver.GetUnresolved(module,
                     createdContext, requiredDep, userPath.Select(Path.GetDirectoryName).ToArray());
             }
+
             return requiredDep;
         }
 
         /// <summary>
-        /// Make a config file 
+        ///     Make a config file
         /// </summary>
         /// <param name="type">type of the config file : application or project</param>
         /// <param name="path">Path were config file is created</param>
@@ -265,52 +226,44 @@ namespace ShieldCLI.Commands
         public void ConfigMakeFile(string type, string path, string preset, string name)
 
         {
-            string[] presets = { "maximum", "balance", "custom", "optimized" };
+            string[] presets = {"maximum", "balance", "custom", "optimized"};
 
-            string[] protectionsId = { "protrection1", "protection2" };
+            string[] protectionsId = {"protrection1", "protection2"};
 
             if (type != "application" && type != "project")
-            {
                 type = AnsiConsole.Prompt(
-                         new SelectionPrompt<string>()
-                         .Title("[white]Please choose the type of protection[/]?")
-                         .PageSize(3)
-                         .AddChoice("project")
-                         .AddChoice("application"));
-            }
+                    new SelectionPrompt<string>()
+                        .Title("[white]Please choose the type of protection[/]?")
+                        .PageSize(3)
+                        .AddChoice("project")
+                        .AddChoice("application"));
 
             if (presets.All(pr => pr != preset))
-            {
                 preset = AnsiConsole.Prompt(
                     new SelectionPrompt<string>()
                         .Title("[white]Please choose the preset for the protection of protection[/]?")
                         .PageSize(4)
                         .AddChoices(presets));
 
-            }
-
             if (type == "application")
             {
-                var applicationConfig = preset.Equals("custom") ? ClientManager.Client.Configuration.MakeApplicationCustomConfiguration(protectionsId) :
-                    ClientManager.Client.Configuration.MakeApplicationConfiguration(preset.ToPreset());
+                var applicationConfig = preset.Equals("custom")
+                    ? ClientManager.Client.Configuration.MakeApplicationCustomConfiguration(protectionsId)
+                    : ClientManager.Client.Configuration.MakeApplicationConfiguration(preset.ToPreset());
 
                 applicationConfig.SaveToFile(path, name);
             }
             else
             {
-                var projectConfig = preset.Equals("custom") ? ClientManager.Client.Configuration.MakeProjectCustomConfiguration(protectionsId) :
-                    ClientManager.Client.Configuration.MakeProjectConfiguration(preset.ToPreset());
+                var projectConfig = preset.Equals("custom")
+                    ? ClientManager.Client.Configuration.MakeProjectCustomConfiguration(protectionsId)
+                    : ClientManager.Client.Configuration.MakeProjectConfiguration(preset.ToPreset());
 
                 projectConfig.SaveToFile(path, name);
             }
 
 
             AnsiConsole.Markup("[lime]Configuration file created sucessfully.[/]");
-
-
-
-
         }
     }
 }
-
